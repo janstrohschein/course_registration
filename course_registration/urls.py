d729d77a1b03ba377285d9bf6ac533b89fb8e11e
Jan Strohschein <strohschein@gmx.net>
2017-01-07 14:17:02 +0100

    fixed pagination error in url file

--- course_registration/urls.py
+++ course_registration/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^courses', views.CourseList.as_view(), name='course_list'),
+    url(r'^courses$', views.CourseList.as_view(), name='course_list'),
     url(r'^courses/(?P<page>\d+)$', views.CourseList.as_view(), name='course_list'),
     url(r'^course/(?P<slug>[-\w]+)/$', views.CourseDetail.as_view(), name='course_detail'),
 ]
